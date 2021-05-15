26dcf7c4a911cef986a3499685c18f764f7fea76
Joe Longstreet <joelongstreet@gmail.com>
2021-05-15 09:49:04 -0500

    feat: add a count by day route

--- app/api/pizzas_consumptions.rb
+++ app/api/pizzas_consumptions.rb
@@ -3,5 +3,24 @@ class Garfield::API::Pizzas_Consumptions < Grape::API
     get do
       Garfield::Models::Pizzas_Consumptions.all
     end
+
+    params do
+      optional :month_id, type: Integer
+    end
+    get '/count-by-day' do
+      data_set = Garfield::Models::Pizzas_Consumptions
+        .group_by_eaten_at
+        .order_by_count
+      
+      if (params.key?(:month))
+        data_set = data_set.filter_by_month(params[:month])
+      end
+
+      if (params.key?(:year))
+        data_set = data_set.filter_by_year(params[:year])
+      end
+
+      data_set.naked
+    end
   end
 end
--- app/models/pizzas_consumptions.rb
+++ app/models/pizzas_consumptions.rb
@@ -1,4 +1,21 @@
 class Garfield::Models::Pizzas_Consumptions < Sequel::Model
   many_to_one :pizza
   many_to_one :person
+
+  dataset_module do
+    group_and_count(:group_by_eaten_at, :eaten_at)
+    order(:order_by_count, Sequel.desc(:count))
+    
+    def filter_by_month(month)
+      where(
+        Sequel[:eaten_at].extract(:month) =~ month
+      )
+    end
+
+    def filter_by_year(year)
+      where(
+        Sequel[:eaten_at].extract(:year) =~ year
+      )
+    end
+  end
 end
